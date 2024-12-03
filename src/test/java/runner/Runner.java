e12e295fd8d65ab9b4a1e8aeb5b68952b10c31c4
M Yalcin <yalcinmusa60@gmail.com>
2024-12-03 20:51:18 +0100

    First test on Jenkins

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -19,7 +19,7 @@ import org.junit.runner.RunWith;
         monochrome = false,//Daha düzenli log işlemi için
         features = "src/test/resources/features",//Feature dosyalarının yolu buraya belirtilmelidir
         glue = "steps",//Çalıştırılacak Java classlarının yolu buraya belirtilmelidir
-        tags = "@candidat",//Özel olarak çalıştırılacak Scenario yada Feature Tag'i buraya belirtilir
+        tags = "@davidson",//Özel olarak çalıştırılacak Scenario yada Feature Tag'i buraya belirtilir
         dryRun =false  //dryRun = true --> Testi çalıştırmaz, eksik step definitionları tespit eder.
 )
 
